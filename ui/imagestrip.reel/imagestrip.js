7846d19aafc53ec17e88e803debe0a020a7a3c98
Radikzz <noreply>
2015-10-18 13:45:49 +0000

    Add component imagestrip

---
+++ ui/imagestrip.reel/imagestrip.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/imagestrip.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Imagestrip
+ * @extends Component
+ */
+exports.Imagestrip = Component.specialize(/** @lends Imagestrip# */ {
+    constructor: {
+        value: function Imagestrip() {
+            this.super();
+        }
+    }
+});
