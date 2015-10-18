0fb5b486dc4faa044e963f82f5e0d5a00f62e512
Radikzz <noreply>
2015-10-18 16:06:29 +0000

    Update file core/flickr-service.js

--- core/flickr-service.js
+++ core/flickr-service.js
@@ -6,3 +6,5 @@ var Montage = require("montage/core/core").Montage;
 var RangeController = require("montage/core/range-controller").RangeController;
 
 var API_KEY = "97ec626f4dcbf03e6b645b2e7da5571e";
+var IMAGE = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)";
+
