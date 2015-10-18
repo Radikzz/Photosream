ccc914ef10bc3bd27cdd7fd59e69f4ae53c7193f
Radikzz <noreply>
2015-10-18 22:03:20 +0000

    Update file core/flickr-service.js

--- core/flickr-service.js
+++ core/flickr-service.js
@@ -13,6 +13,12 @@ var IMAGE = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_
 *class FlickrService
 * @extends Montage
 **/
-exportsd.FlickrService = Montage.specilaze(/** @lends FlickrService# */ {
-    //TODO combine constructor and load
+exportsd.FlickrService = Montage.specialaze(/** @lends FlickrService# */ {
+    /*TODO combine constructor and load*/
+    constructor: {
+        value: function() {
+            var self = this;
+        }
+    }
 })
+exports.shared = new exports.FlickrService();
