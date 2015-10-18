c8bac0164ad0f62ea79caa8dda5f80072efdfe2d
Radikzz <noreply>
2015-10-18 13:49:26 +0000

    Add component image

---
+++ ui/imagestrip.reel/image.reel/image.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/imagestrip.reel/image.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Image
+ * @extends Component
+ */
+exports.Image = Component.specialize(/** @lends Image# */ {
+    constructor: {
+        value: function Image() {
+            this.super();
+        }
+    }
+});
