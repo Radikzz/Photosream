e26ee9d130751155b24ee5616ec4753da0540fbf
Radikzz <noreply>
2015-10-18 16:03:16 +0000

    Update file core/flickr-service.js

---
+++ core/flickr-service.js
@@ -0,0 +1,8 @@
+/**
+ * @module ./flickr-service
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+var RangeController = require("montage/core/range-controller").RangeController;
+
+var API_KEY = "97ec626f4dcbf03e6b645b2e7da5571e";
