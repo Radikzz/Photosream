088eacec9fe3d5a72841992db6916eb5bd7ae3bc
Radikzz <noreply>
2015-10-18 16:43:58 +0000

    Update file core/flickr-service.js

--- core/flickr-service.js
+++ core/flickr-service.js
@@ -8,3 +8,11 @@ var RangeController = require("montage/core/range-controller").RangeController;
 var API_KEY = "97ec626f4dcbf03e6b645b2e7da5571e";
 var IMAGE = "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)";
 
+
+/**
+*class FlickrService
+* @extends Montage
+**/
+exportsd.FlickrService = Montage.specilaze(/** @lends FlickrService# */ {
+    //TODO combine constructor and load
+})
